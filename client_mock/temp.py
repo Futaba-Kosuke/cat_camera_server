e20b3d77fe3ea7d08258703ecd242f5cbb474523
二場昴輔 <futaba@nijousubarutasukunoMacBook-Pro.local>
2020-11-07 17:44:31 +0900

    Add: GPUサーバーへのデータ受け渡しを実装 (#3)

---
+++ client_mock/temp.py
@@ -0,0 +1,37 @@
+import sys
+import json
+import base64
+import requests
+import cv2
+import os
+import glob
+
+url = sys.argv[1]
+
+def numpy_to_base64(img_np):
+    _, temp = cv2.imencode('.jpeg', img_np)
+    img_base64 = base64.b64encode(temp)
+    return img_base64
+
+paths = glob.glob(os.path.join('./datasets', '*', '**', '*'), recursive=True)
+
+payload = {
+    'data_list': [{}] * len(paths),
+}
+for i, path in enumerate(paths):
+    img = cv2.imread(path)
+    img_base64 = numpy_to_base64(img)
+    data = {
+        'img': img_base64.decode('utf-8'),
+        'label': os.path.basename(os.path.dirname(path))
+    }
+    payload['data_list'][i] = data
+
+payload = json.dumps(payload).encode("utf-8")
+
+headers = {"Content-Type": "application/json"}
+
+response = requests.post('{}/training'.format(url), headers=headers, data=payload)
+
+with open('./temp.zip', 'wb') as f:
+    f.write(response.content)
